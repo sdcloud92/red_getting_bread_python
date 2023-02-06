07ffbbb7d9dff30f16d3b40c04c18e9d766914d6
Shomarri Diaz <shomarri.diaz@gmail.com>
2023-02-06 07:00:48 +0000

    codeacademy exercises

---
+++ Input_Function.py
@@ -0,0 +1,12 @@
+# The input function gives a prompt to the user to type in some data
+
+#eg
+# input("What is your name? ")
+# input("How old are you? ")
+
+#Combining input and print
+#eg
+# print("Hello" + input("What is your name? "))
+
+print("Hello " + input("What is your name? "))
+print("Wow you are " + input("How old are you? ") + " years old")
