6c0cf26337cc65a751bb2ccd372ee11da20c015f
Shomarri Diaz <shomarri.diaz@gmail.com>
2023-02-07 05:54:25 +0000

    variables

---
+++ Variables.py
@@ -0,0 +1,16 @@
+#input("What is your name? ")
+# If i give the above function a name i can refer to it later on
+
+#name = input("What is your name? ")
+#print(name)
+
+# You can change varibales
+
+#name = "Sho"
+#print(name)
+
+name = input("What is your name? ")
+length = len(name)
+print(length)
+
+
