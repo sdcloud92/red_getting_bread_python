6d5aa8f14901f31aefb1bab6e1120b5eff7e2f5c
Shomarri Diaz <shomarri.diaz@gmail.com>
2023-02-07 22:30:59 +0000

    data types

---
+++ data_types.py
@@ -0,0 +1,46 @@
+# You CAN count number of characters in a string print(len("Hello"))
+# This you CANNOT count how many digits = 'Type error' print(len(123452))
+
+#string "Hello" must use ""
+# print("Hello"[0])
+
+#subscripting []
+
+#Integer (means whole number)
+# print(123 + 345)
+
+#large integer 342_654_896
+
+#float is decimal numbers 4.14159(pi)
+
+#Boolean True/False
+
+#type() will check what the date type is
+# num_char = len(input("What is your name? "))
+# print("Your name has " + num_char + " characters.")
+
+# print(type(num_char)) #type checking
+
+#type casting
+
+# How to turn integer into a string
+
+# num_char = len(input("What is your name? "))
+
+# new_num_char = str(num_char)
+
+# print("Your name has " + new_num_char + " characters.")
+
+# a = 123
+# print(type(a))
+
+# new_a = str(a)
+# print(new_a)
+
+# a = str(123)
+# print(type(a))
+
+a = float(123)
+print(type(a))
+
+print(70 + float("100.5"))
