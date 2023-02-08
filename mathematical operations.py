628b5cca19efae0ee31a5e0cb93f3e40bf952cb3
Shomarri Diaz <shomarri.diaz@gmail.com>
2023-02-08 05:16:13 +0000

    mathematical operations

---
+++ mathematical operations.py
@@ -0,0 +1,15 @@
+# 3 + 5
+# 7 - 4 
+# 3 * 2
+# 6 / 3 # will always display as a float
+# 2 ** 2 #2 to the power of 2
+
+# PEMDASLR #Order of priority operations
+# Parentheses ()
+# Exponents **
+# Multiplication * Divisiion / 
+# Addition + Subtraction -
+
+print(3 * 3 + 3 / 3 - 3)
+print(3 * (3 + 3) / 3 - 3)
+
