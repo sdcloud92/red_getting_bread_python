7f26351e4b3c5feccce146158449501333271de7
Shomarri Diaz <shomarri.diaz@gmail.com>
2023-02-08 16:28:21 +0000

    if/else conditions lesson

---
+++ if/else_conditionals.py
@@ -0,0 +1,29 @@
+# if condition:
+#     do this
+# else:      
+#     do this
+
+#eg
+# water_level = 50
+# if water_level > 80:
+#     print("Drain Water")
+# else:
+#     print("Continue")
+
+    # > Greater than
+    # < Less than
+    # >= Greater than or equal to
+    # <= Less than or equal to
+    # = Assignment
+    # == Equal to
+    # != Not equal to 
+    # % modulo tells what the remainder of a division is eg 7 % 1 = 1..... 2 + 2 + 2 + 1
+
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+    print("Can ride")
+else:
+    print("Can't ride...sorry")
+    
